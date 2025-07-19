8a602912bc5e74f3baa83d8016ee45ad771e2c63
vishalnikhil <vishalnikhil2002@gmail.com>
2025-07-20 02:50:11 +0530

    till here outlet to fix navbar everywhere footter can be ddone like this only login page api integration cors error fixed url fixing between frotend and bckend

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 
 const Login = () => {
-  return (
-    <div>
 
-        login
+
+      const [emailId,setEmailId]=useState("suresh@gmail.com");
+
+      const [password,setPassword]=useState("Suresh@143");
+
+    //   console.log(emailId);
+    //   console.log(Password);
+
+      const handleLogin = async ()=>  {   //this will make an api call when we click on the login page
+
+
+            try{ 
+              
+              const res = await axios.post("http://localhost:7777/login",{
+                emailId,
+                password,
+             },{withCredentials:true})
+            }
+            catch(err){
+
+                 console.log(err);
+
+            }
+
+      }
+
+
+
+
+  return (
+    <div className='flex justify-center my-10'>
+
+        <div className="card bg-base-300 w-96 shadow-sm">
+  <div className="card-body">
+    <h2 className="card-title justify-center">Login page</h2>
+
+
+       <div className='m-2'>
+
+        <fieldset className="fieldset ">
+  <legend className="fieldset-legend">Email Id</legend>
+  <input 
+  type="text" 
+  value={emailId} 
+   onChange={(e)=>{setEmailId(e.target.value)}}
+  className="input"  />
+
+</fieldset>
+
+
+
+ <fieldset className="fieldset">
+  <legend className="fieldset-legend">Password</legend>
+  <input type="text" onChange={(e)=>{setPassword(e.target.value)}} value={password} className="input"  />
+
+</fieldset>
+
+       </div>
+
+
+
+
+    <div className="card-actions flex justify-center">
+      <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+    </div>
+  </div>
+</div>
       
     </div>
   )
